658c45de17fad47900e5db5c1c6dd50aebd668e9
kajal7906 <153822811+kajal7906@users.noreply.github.com>
2024-01-04 19:39:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const press = ()=>{
     var image = document.querySelector("#imageId")
     var headingEle = document.getElementById("declaration") 
     switch(randomX){
-        case 1 : image.setAttribute("src","diceImages/1.svg")
+        case 1 : image.setAttribute("src","diceimages/1.svg")
                  headingEle.innerHTML = "1"   
                  break
-        case 2 : image.setAttribute("src","diceImages/2.svg")
+        case 2 : image.setAttribute("src","diceimages/2.svg")
                  headingEle.innerHTML = "2"   
                  break          
-        case 3 : image.setAttribute("src","diceImages/3.svg")
+        case 3 : image.setAttribute("src","diceimages/3.svg")
                  headingEle.innerHTML = "3"   
                  break
-        case 4 : image.setAttribute("src","diceImages/4.svg")
+        case 4 : image.setAttribute("src","diceimages/4.svg")
                  headingEle.innerHTML = "4"   
                  break                 
-        case 5 : image.setAttribute("src","diceImages/5.svg")
+        case 5 : image.setAttribute("src","diceimages/5.svg")
                  headingEle.innerHTML = "5"   
                  break
-        case 6 : image.setAttribute("src","diceImages/6.svg")
+        case 6 : image.setAttribute("src","diceimages/6.svg")
                  headingEle.innerHTML = "6"   
                  break
         default: headingEle.innerHTML = "please choose 1 to 6"            
